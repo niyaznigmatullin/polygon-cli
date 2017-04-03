8d7ebb11018d175de8d39ebd8c305929663d3c16
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2017-04-03 13:22:25 +0300

    fix for pep8

--- polygon_cli/actions/import_folder.py
+++ polygon_cli/actions/import_folder.py
@@ -1,5 +1,5 @@
 from .common import *
-import os
+
 
 def process_import_problem_from_folder(directory):
     if not load_session():
@@ -7,10 +7,14 @@ def process_import_problem_from_folder(directory):
     global_vars.problem.import_problem_from_folder(directory)
     save_session()
 
+
 def add_parser(subparsers):
     parser_import_problem_from_folder = subparsers.add_parser(
             'import_folder',
             help="Imports problem from folder"
     )
-    parser_import_problem_from_folder.add_argument('directory', help='The directory that is going to be imported. It has to contain `src` and `solutions` folders')
-    parser_import_problem_from_folder.set_defaults(func=lambda options: process_import_problem_from_folder(options.directory))
+    parser_import_problem_from_folder.\
+        add_argument('directory', help='The directory that is going to be imported. '
+                                       'It has to contain `src` and `solutions` folders')
+    parser_import_problem_from_folder.\
+        set_defaults(func=lambda options: process_import_problem_from_folder(options.directory))
